a1939ca19e10723ecdab6c854b9c82dc94dd7f88
Alessio Limina <alessio.limina@gmail.com>
2023-01-30 18:56:53 +0100

    Exercise: arrays-1

--- src/it/develhope/arraysAndLists/Start.java
+++ src/it/develhope/arraysAndLists/Start.java
@@ -9,27 +9,18 @@ public class Start {
         System.out.println("--------------Creating an array of cars-----------------------");
 
         String[] carBrands = new String[]{"Fiat", "BMW", "Mercedes", "Volkswagen", "Toyota"};
-
         System.out.println(carBrands.length);
-
         System.out.println(carBrands[2]);
 
         System.out.println("---------------Creating an array of prime numbers--------------");
 
         int[] primeNumbers = new int[6];
-
         primeNumbers[0] = 2;
-
         primeNumbers[1] = 3;
-
         primeNumbers[2] = 5;
-
         primeNumbers[3] = 7;
-
         primeNumbers[4] = 11;
-
         primeNumbers[5] = 13;
-
         System.out.println(Arrays.toString(primeNumbers));
 
         System.out.println("---------------------------------------------------------------");
